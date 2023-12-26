30a247367c9997149680922ce91426009d74f08b
Dmitriy Lebedev <dante1213@yandex.ru>
2023-12-26 21:07:51 +0300

    Исправлена константа

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../utils/errors/Unauthorized');
-const NeedAuthrization = require('../utils/constants/constants');
+const NEED_AUTHORIZATION = require('../utils/constants/constants');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new Unauthorized(NeedAuthrization);
+    throw new Unauthorized(NEED_AUTHORIZATION);
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new Unauthorized(NeedAuthrization);
+    throw new Unauthorized(NEED_AUTHORIZATION);
   }
 
   req.user = payload;
